7490c5259cbcf7c17ed145c8582fff39c4180f86
Olaniel <olaniel@mail.ru>
2021-03-15 21:35:06 +0300

    Add files via upload

---
+++ jc1-les02/src/by/htp/les02/main/Operator.java
@@ -0,0 +1,35 @@
+package by.htp.les02.main;
+
+public class Operator {
+
+	public static void main(String[] args) {
+
+		int a = 1;
+		int b = 0;
+		int c = 12;
+		
+		
+		if (b == 0) {
+			System.out.println();
+		}
+
+		
+		if (a  == 0 ) {
+			System.out.println("asd");
+		} else {
+			System.out.println("qwe");
+		}
+		
+		
+		
+		if (a != 0 ) {// if ( !(a  == 0 ))
+			System.out.println("qwe");
+		} else {
+			System.out.println("asd");
+		}
+
+		System.out.println("!!!!!");
+
+	}
+
+}
